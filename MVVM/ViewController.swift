73cb20dc84bfd42b8decbe53fe58220788fc37b3
Rafael <rafael.glez@gmail.com>
2025-08-02 07:17:50 -0600

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Rafael Gonzalez on 02/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
